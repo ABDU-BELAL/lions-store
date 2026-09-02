00f1f293fffeaed1031b2d84ce0f35ca14d105a2
Abdulrahman M.Belal <Omomar.yasso1@gmail.com>
2026-09-02 15:11:29 +0300

    Add capture attribute to file input for KYC

--- src/routes/kyc.tsx
+++ src/routes/kyc.tsx
@@ -170,6 +170,7 @@ function Kyc() {
                 <input
                   type="file"
                   accept="image/*"
+                  capture="environment"
                   className="hidden"
                   onChange={async (e) => {
                     const f = e.target.files?.[0] ?? null;
